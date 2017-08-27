a216ed0cbaa071c8ba2e6b2c6be803cf055682b7
Mark Rendle <mark@rendle.io>
2017-08-27 12:50:33 +0100

    Failing

---
+++ ResourceRepro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ResourceRepro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Properties.Resources.Message);
+        }
+    }
+}
